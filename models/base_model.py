e783e43531433b75195dab2659e47f304f22f663
Adeniyii <ifedayoadeniyi@gmail.com>
2022-08-02 19:41:17 +0100

    fix: implement proper serialization/deserialization of objects
    Most of the previous errors were around incomplete implementations,
    and wrong return values. Also, private class attributes were being
    set using `self` instead of the class name.
    
    Added a call to the `FileStorage` method: `new()` when no kwargs
    are provided to the `BaseModel` instantiation.
    
    Made other atomic fixes.

--- models/base_model.py
+++ models/base_model.py
@@ -2,40 +2,44 @@
 """
 Module: base.py
 """
+import models
 import uuid
-import re
 from datetime import datetime
-from models import storage
+
 
 class BaseModel():
     """
     Base class which defines all common
     attributes/methods for other classes
     """
+
     def __init__(self, *args, **kwargs):
         """
         instatiates an object with it's
         attributes
         """
-        re.compile('/d{2,4}-/d{2,4}-(/d{2}-)*
-        if kwargs:
+        if len(kwargs) > 0:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key.endswith('ted_at'):
-                        value = datetime.fromisoformat(value)
-                    setattr(self,key,value)
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
             return
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        models.storage.new(self)
 
     def __str__(self):
         """
         Returns the string representation
         of the instance
         """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -43,16 +47,16 @@ class BaseModel():
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
         returns a dictionary containing all keys/values
         of __dict__ of the instance
         """
-        to_dict = self.__dict__
-        to_dict['__class__'] = type(self).__name__
-        to_dict['created_at'] = to_dict['created_at'].isoformat()
-        to_dict['updated_at'] = to_dict['updated_at'].isoformat()
+        dict = self.__dict__
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
 
-        return to_dict
+        return dict
