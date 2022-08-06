d914fab4b6c784f4c9e54a080cb6034412fdb809
cecilia-89 <catabong89@gmail.com>
2022-08-06 07:58:12 +0100

    modified: 'user' testcase

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,11 +14,39 @@ class TestState(unittest.TestCase):
 	"""Test cases for the `User` class."""
 
 	def test_params(self):
-		k = f"{type(a1).__name__}.{a1.id}"
-		self.assertIsInstance(u1.email, "")
+		k = f"{type(u1).__name__}.{u1.id}"
+		self.assertIn(k, storage.all())
+		self.assertIsInstance(u1.email, str)
 		self.assertIsInstance(u1.password, str)
 		self.assertIsInstance(u1.first_name, str)
 		self.assertIsInstance(u1.last_name, str)
+
+	def test_init(self):
+		"""Test method for public instances"""
+		self.assertIsInstance(u1.id, str)
+		self.assertIsInstance(u1.created_at, datetime)
+		self.assertIsInstance(u1.updated_at, datetime)
+		self.assertEqual(u1.updated_at, u2.updated_at)
+
+	def test_str(self):
+		"""Test method for str representation"""
+		string = f"[{type(u1).__name__}] ({u1.id}) {u1.__dict__}"
+		self.assertEqual(u1.__str__(), string)
+
+	def test_save(self):
+		"""Test method for save"""
+		old_update = u1.updated_at
+		u1.save()
+		self.assertNotEqual(u1.updated_at,old_update)
+
+	def test_todict(self):
+		"""Test method for dict"""
+		a_dict = u2.to_dict()
+		self.assertIsInstance(a_dict, dict)
+		self.assertEqual(a_dict['__class__'], type(u2).__name__ )
+		self.assertIn('created_at', a_dict.keys())
+		self.assertIn('updated_at', a_dict.keys())
+		self.assertNotEqual(u1, u2)
 
 if __name__ == "__main__":
 	unittest.main()
