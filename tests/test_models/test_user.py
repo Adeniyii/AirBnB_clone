ce14a6ffd570fa6faf0d925bb48d92e2b8fb7b63
cecilia-89 <catabong89@gmail.com>
2022-08-06 07:52:24 +0100

    modified: 'user' testcase

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,17 +3,22 @@
 """
 import unittest
 from models.user import User
+from models import storage
+from datetime import datetime
 
 u1 = User()
+u2 = User(**u1.to_dict())
+u3 = User("hello", "wait", "in")
 
 class TestState(unittest.TestCase):
 	"""Test cases for the `User` class."""
 
 	def test_params(self):
-		self.assertEqual(u1.email, "")
-		self.assertEqual(u1.password, "")
-		self.assertEqual(u1.first_name, "")
-		self.assertEqual(u1.last_name, "")
+		k = f"{type(a1).__name__}.{a1.id}"
+		self.assertIsInstance(u1.email, "")
+		self.assertIsInstance(u1.password, str)
+		self.assertIsInstance(u1.first_name, str)
+		self.assertIsInstance(u1.last_name, str)
 
 if __name__ == "__main__":
 	unittest.main()
