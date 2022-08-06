a8a57572a4e2935694c7d35ba546edf5fde2489a
cecilia-89 <catabong89@gmail.com>
2022-08-06 08:17:14 +0100

    refactor to fit pycodestyle

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -10,11 +10,13 @@ a1 = Amenity()
 a2 = Amenity(**a1.to_dict())
 a3 = Amenity("hello", "wait", "in")
 
+
 class TestAmenity(unittest.TestCase):
     """Test cases for the `Amenity` class."""
 
     def test_params(self):
         """Test method for class attributes"""
+
         k = f"{type(a1).__name__}.{a1.id}"
         self.assertIsInstance(a1.name, str)
         self.assertIn(k, storage.all())
@@ -22,6 +24,7 @@ class TestAmenity(unittest.TestCase):
 
     def test_init(self):
         """Test method for public instances"""
+
         self.assertIsInstance(a1.id, str)
         self.assertIsInstance(a1.created_at, datetime)
         self.assertIsInstance(a1.updated_at, datetime)
@@ -29,24 +32,27 @@ class TestAmenity(unittest.TestCase):
 
     def test_str(self):
         """Test method for str representation"""
+
         string = f"[{type(a1).__name__}] ({a1.id}) {a1.__dict__}"
         self.assertEqual(a1.__str__(), string)
 
     def test_save(self):
         """Test method for save"""
+
         old_update = a1.updated_at
         a1.save()
-        self.assertNotEqual(a1.updated_at,old_update)
+        self.assertNotEqual(a1.updated_at, old_update)
 
     def test_todict(self):
         """Test method for dict"""
+
         a_dict = a2.to_dict()
         self.assertIsInstance(a_dict, dict)
-        self.assertEqual(a_dict['__class__'], type(a2).__name__ )
+        self.assertEqual(a_dict['__class__'], type(a2).__name__)
         self.assertIn('created_at', a_dict.keys())
         self.assertIn('updated_at', a_dict.keys())
         self.assertNotEqual(a1, a2)
 
-if __name__ == "__main__":
-	unittest.main()
 
+if __name__ == "__main__":
+    unittest.main()
