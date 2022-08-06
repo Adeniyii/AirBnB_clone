1c88ab77ca5712972db4f610ba3e05e064ae9ef8
Adeniyii <ifedayoadeniyi@gmail.com>
2022-08-06 20:26:23 +0100

    feat: add extra classes

--- console.py
+++ console.py
@@ -31,8 +31,15 @@ import json
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
 
-current_classes = {'BaseModel': BaseModel, 'User': User}
+current_classes = {'BaseModel': BaseModel, 'User': User,
+                   'Amenity': Amenity, 'City': City, 'State': State,
+                   'Place': Place, 'Review': Review}
 
 
 class HBNBCommand(cmd.Cmd):
