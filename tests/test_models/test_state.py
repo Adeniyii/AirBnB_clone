4faa8f058a8fe89938c8d410961cfbb0a4093d15
cecilia-89 <catabong89@gmail.com>
2022-08-06 07:41:41 +0100

    modified:  testcase

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -3,16 +3,20 @@
 """
 import unittest
 from models.state import State
+from models import storage
+from datetime import datetime
 
 s1 = State()
+a2 = State(**a1.to_dict())
+a3 = State("hello", "wait", "in")
 
 class TestState(unittest.TestCase):
     """Test cases for the `State` class."""
 
     def test_params(self):
-        self.assertEqual(s1.name, "")
-        s1.name = "Abuja"
-        self.assertEqual(s1.name, "Abuja")
+        self.assertIsInstance(s1.name, str)
+        s1.name = "Chicago"
+        self.assertEqual(s1.name, "Chicago")
 
 if __name__ == "__main__":
 	unittest.main()
