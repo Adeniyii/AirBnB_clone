927279e143579ce5bfb804f467bcef32c51fd169
cecilia-89 <catabong89@gmail.com>
2022-08-06 08:10:12 +0100

    refactor to fit pycodestyle

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
-#usr/bin/python3
+#!usr/bin/python3
 from models.base_model import BaseModel
 
-class User(BaseModel):
 
-	email = ""
-	password = ""
-	first_name = ""
-	last_name = ""
+class User(BaseModel):
+    """Creates a new user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
